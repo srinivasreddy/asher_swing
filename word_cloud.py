03695f9d1bf5ad05c5a62c6a9908a4fdedd0fd60
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2020-01-29 18:20:01 +0530

    Formatted with black and changes has been done

--- word_cloud.py
+++ word_cloud.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
 
 import xlrd
-from wordcloud import WordCloud, STOPWORDS
 import numpy as np
 import matplotlib.pyplot as plt
-from PIL import Image
 from wordcloud import WordCloud, STOPWORDS
-from IPython.display import Image as im
 from collections import Counter
 import re
 
+
 def _clean_data(raw_string):
-    no_links = re.sub(r'http\S+', '', raw_string)
-    no_unicode = re.sub(r"\\[a-z][a-z]?[0-9]+", '', no_links)
-    no_special_characters = re.sub('[^A-Za-z ]+', '', no_unicode)
+    no_links = re.sub(r"http\S+", "", raw_string)
+    no_unicode = re.sub(r"\\[a-z][a-z]?[0-9]+", "", no_links)
+    no_special_characters = re.sub("[^A-Za-z ]+", "", no_unicode)
     return no_special_characters
+
 
 def _generate_word_counter(sheet):
     nrows = sheet.nrows
@@ -22,21 +21,37 @@ def _generate_word_counter(sheet):
     for row in range(2, nrows):
         _count = sheet.cell(row, 1).value
         clean_data = _clean_data(sheet.cell(row, 4).value)
-        temp_counter.update({word.lower():_count  for word in clean_data.split() if word not in STOPWORDS})
+        temp_counter.update(
+            {
+                word.lower(): _count
+                for word in clean_data.split()
+                if word not in STOPWORDS
+            }
+        )
     return temp_counter
 
-def generate_word_cloud():
+
+def generate_word_cloud(freq_counter, file_name):
+    wordcloud = WordCloud(max_words=2000).fit_words(freq_counter)
+    plt.imshow(wordcloud, interpolation="bilinear")
+    plt.axis("off")
+    plt.show()
+
+
+def _main():
     wb = xlrd.open_workbook("video_activity_metrics_Reuters_20200121_20200127_en.xlsx")
-    india_sheet = wb.sheet_by_name('India')
-    latin_america_sheet = wb.sheet_by_name('Latam')
-    reuters_sheet = wb.sheet_by_name('Reuters')
-    business_sheet = wb.sheet_by_name('Biz')
-    japan_sheet = wb.sheet_by_name('Japan')
+    india_sheet = wb.sheet_by_name("India")
+    latin_america_sheet = wb.sheet_by_name("Latam")
+    reuters_sheet = wb.sheet_by_name("Reuters")
+    business_sheet = wb.sheet_by_name("Biz")
+    japan_sheet = wb.sheet_by_name("Japan")
     india_dict = _generate_word_counter(india_sheet)
-    import pdb; pdb.set_trace()
+    generate_word_cloud(india_dict, "India")
+    business_dict = _generate_word_counter(business_sheet)
+    generate_word_cloud(business_dict, "Biz")
+    reuters_dict = _generate_word_counter(reuters_sheet)
+    generate_word_cloud(reuters_dict, "Reuters")
 
 
 if __name__ == "__main__":
-    generate_word_cloud()
-
-
+    _main()
